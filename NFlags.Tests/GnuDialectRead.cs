6b9f7d7ba0e91dcc93feea1906c2c5e3baaecdd6
Bartosz Gołek <bartosz.golek@gmail.com>
2017-11-13 23:31:01 +0100

    fixed test assertions travis-ci configuration

--- NFlags.Tests/GnuDialectRead.cs
+++ NFlags.Tests/GnuDialectRead.cs
@@ -1,3 +1,4 @@
+using NFlags.Commands;
 using Xunit;
 
 namespace NFlags.Tests
@@ -20,107 +21,126 @@ namespace NFlags.Tests
         [Fact]
         public void TestParams_ShouldSetDefaultOptionValue_IfNotContainedInArgs()
         {
+            CommandArgs a = null;
+
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
                 .Root(configurator => configurator
                     .RegisterOption("option", "o", "", "defaultValue")
-                    .SetExecute((args, output) => Assert.Equal(args.Options["option"], "defaultValue"))
+                    .SetExecute((args, output) => a = args)
                 )
                 .Run(new string[] { });
+
+            Assert.Equal(a.Options["option"], "defaultValue");
         }
 
         [Fact]
         public void TestParams_ShouldSetMultipleOptionValues_IfContainedInArgs()
         {
+            CommandArgs a = null;  
+            
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
                 .Root(configurator => configurator
                     .RegisterOption("option1", "o1", "", "")
                     .RegisterOption("option2", "o2", "", "")
-                    .SetExecute((args, output) =>
-                    {                        
-                        Assert.Equal(args.Options["option1"], "optionValue1");
-                        Assert.Equal(args.Options["option2"], "optionValue2");
-                    })
+                    .SetExecute((args, output) => a = args)
                 )
-                .Run(new[] {"--option1", "optValue1", "--option2", "optValue2"});
+                .Run(new[] {"--option1", "optionValue1", "--option2", "optionValue2"});
+            
+            Assert.Equal(a.Options["option1"], "optionValue1");
+            Assert.Equal(a.Options["option2"], "optionValue2");
         }
 
         [Fact]
         public void TestParams_ShouldSetOptionValue_IfAbrContainedInArgs()
         {
+            CommandArgs a = null;  
+            
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
                 .Root(configurator => configurator
                     .RegisterOption("option", "o", "", "")
-                    .SetExecute((args, output) => Assert.Equal(args.Options["option1"], "optionValue1"))
+                    .SetExecute((args, output) => a = args)
                 )
-                .Run(new[] {"-o", "optValue"});
+                .Run(new[] {"-o", "optionValue"});
+
+            Assert.Equal(a.Options["option"], "optionValue");
         }
 
         [Fact]
         public void TestParams_ShouldSetMultipleOptionValues_IfAbrContainedInArgs()
         {
+            CommandArgs a = null;  
+            
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
                 .Root(configurator => configurator
                     .RegisterOption("option1", "o1", "", "")
                     .RegisterOption("option2", "o2", "", "")
-                    .SetExecute((args, output) =>
-                    {                        
-                        Assert.Equal(args.Options["option1"], "optionValue1");
-                        Assert.Equal(args.Options["option2"], "optionValue2");
-                    })
+                    .SetExecute((args, output) => a = args)
                 )
-                .Run(new[] {"-o1", "optValue1", "-o2", "optValue2"});
+                .Run(new[] {"-o1", "optionValue1", "-o2", "optionValue2"});
+            
+            Assert.Equal(a.Options["option1"], "optionValue1");
+            Assert.Equal(a.Options["option2"], "optionValue2");
         }
 
         [Fact]
         public void TestParams_ShouldSetMultipleOptionValues_IfOptionHasNoAbrAndContainedInArgs()
         {
+            CommandArgs a = null;  
+            
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
                 .Root(configurator => configurator
                     .RegisterOption("option1", "", "")
                     .RegisterOption("option2", "", "")
-                    .SetExecute((args, output) =>
-                    {                        
-                        Assert.Equal(args.Options["option1"], "optionValue1");
-                        Assert.Equal(args.Parameters["option2"], "optionValue2");
-                    })
+                    .SetExecute((args, output) => a = args)
                 )
                 .Run(new[] {"--option1", "optValue1", "--option2", "optValue2"});
+
+            Assert.Equal(a.Options["option1"], "optValue1");
+            Assert.Equal(a.Options["option2"], "optValue2");
         }
 
         [Fact]
         public void TestParams_ShouldSetFlag_IfContainedInArgs()
         {
+            CommandArgs a = null;  
+            
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
                 .Root(configurator => configurator
                     .RegisterFlag("xFlag", "x", "", false)
-                    .SetExecute((args, output) => Assert.True(args.Flags["xFlag"]))
+                    .SetExecute((args, output) => a = args)
                 )
                 .Run(new[] {"--xFlag"});
+
+            Assert.True(a.Flags["xFlag"]);
         }
 
         [Fact]
         public void TestParams_ShouldNotSetFlagValue_IfNotContainedInArgs()
         {
+            CommandArgs a = null;  
+            
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
                 .Root(configurator => configurator
                     .RegisterFlag("yFlag", "y", "", false)
-                    .SetExecute((args, output) => Assert.False(args.Flags["xFlag"]))
+                    .SetExecute((args, output) => a = args)
                 )
                 .Run(new string[] { });
+
+            Assert.False(a.Flags["yFlag"]);
         }
 
         [Fact]
@@ -139,53 +159,56 @@ namespace NFlags.Tests
         [Fact]
         public void TestParams_ShouldReverseFlags_IfContainedInArgs()
         {
+            CommandArgs a = null;  
+            
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
                 .Root(configurator => configurator
                     .RegisterFlag("xFlag1", "x1", "", true)
                     .RegisterFlag("xFlag2", "x2", "", true)
-                    .SetExecute((args, output) =>
-                    {                        
-                        Assert.False(args.Flags["xFlag"]);
-                        Assert.False(args.Flags["xFlag"]);
-                    })
+                    .SetExecute((args, output) => a = args)
                 )
                 .Run(new[] {"--xFlag1", "--xFlag2"});
+            
+            Assert.False(a.Flags["xFlag1"]);
+            Assert.False(a.Flags["xFlag2"]);
         }
 
         [Fact]
         public void TestParams_ShouldSetFlag_IfAbrContainedInArgs()
         {
+            CommandArgs a = null;  
+            
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
                 .Root(configurator => configurator
                     .RegisterFlag("xFlag", "x", "", false)
-                    .SetExecute((args, output) =>
-                    {                        
-                        Assert.True(args.Flags["xFlag"]);
-                    })
+                    .SetExecute((args, output) => a = args)
                 )
                 .Run(new[] {"-x"});
+
+            Assert.True(a.Flags["xFlag"]);
         }
 
         [Fact]
         public void TestParams_ShouldSetMultipleFlags_IfAbrContainedInArgs()
         {
+            CommandArgs a = null;  
+            
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
                 .Root(configurator => configurator
                     .RegisterFlag("flag1", "f1", "", false)
                     .RegisterFlag("flag2", "f2", "", false)
-                    .SetExecute((args, output) =>
-                    {                        
-                        Assert.True(args.Flags["flag1"]);
-                        Assert.True(args.Flags["flag2"]);
-                    })
+                    .SetExecute((args, output) => a = args)
                 )
                 .Run(new[] {"-f1", "-f2"});
+
+            Assert.True(a.Flags["flag1"]);
+            Assert.True(a.Flags["flag2"]);
         }
 
         [Fact]
@@ -216,24 +239,27 @@ namespace NFlags.Tests
         [Fact]
         public void TestParams_ShouldSetSecondParam_IfContainedInArgs()
         {
+            CommandArgs a = null;  
+            
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
                 .Root(configurator => configurator
                     .RegisterParam("param1", "", "")
                     .RegisterParam("param2", "", "")
-                    .SetExecute((args, output) =>
-                    {                        
-                        Assert.Equal(args.Parameters["param1"], "paramValue1");
-                        Assert.Equal(args.Parameters["param2"], "paramValue2");
-                    })
+                    .SetExecute((args, output) => a = args)
                 )
                 .Run(new[] {"paramValue1", "paramValue2"});
+            
+            Assert.Equal(a.Parameters["param1"], "paramValue1");
+            Assert.Equal(a.Parameters["param2"], "paramValue2");
         }
 
         [Fact]
         public void TestParams_ShouldSetSecondFlagsOptionsAndParams()
         {
+            CommandArgs a = null;  
+            
             NFlags.Configure(configurator => configurator
                     .SetDialect(Dialect.Gnu)
                 )
@@ -244,15 +270,7 @@ namespace NFlags.Tests
                     .RegisterOption("option2", "o2", "", "")
                     .RegisterParam("param1", "", "")
                     .RegisterParam("param2", "", "")
-                    .SetExecute((args, output) =>
-                    {                        
-                        Assert.Equal(args.Flags["flag1"], true);
-                        Assert.Equal(args.Flags["flag2"], true);
-                        Assert.Equal(args.Options["option1"], "optionValue1");
-                        Assert.Equal(args.Options["option2"], "optionValue2");
-                        Assert.Equal(args.Parameters["param1"], "paramValue1");
-                        Assert.Equal(args.Parameters["param2"], "paramValue2");
-                    })
+                    .SetExecute((args, output) => a = args)
                 )
                 .Run(new[]
                 {
@@ -263,6 +281,13 @@ namespace NFlags.Tests
                     "paramValue1",
                     "paramValue2"
                 });
+            
+            Assert.Equal(a.Flags["flag1"], true);
+            Assert.Equal(a.Flags["flag2"], true);
+            Assert.Equal(a.Options["option1"], "optionValue1");
+            Assert.Equal(a.Options["option2"], "optionValue2");
+            Assert.Equal(a.Parameters["param1"], "paramValue1");
+            Assert.Equal(a.Parameters["param2"], "paramValue2");
         }
     }
 }
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+set -e
+
+dotnet test ./NFlags.Tests/NFlags.Tests.csproj
