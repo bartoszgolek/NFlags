472e4aacdba26e1f8573743651479c5ecba131af
Bartosz Gołek <bartosz.golek@gmail.com>
2019-09-20 22:47:31 +0200

    Packing script improved

--- pack.sh
+++ pack.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-dotnet pack -c Release NFlags/NFlags.csproj -o $(pwd)
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+TAG=$(git describe --tags)
+IFS='-'
+read -ra VERSION_A <<< "$TAG"
+IFS=' '
+if [[ ${#VERSION_A[@]} -eq 1 ]]; then
+    dotnet pack -c Release NFlags/NFlags.csproj /p:Version=${TAG} -o $(pwd)
+else
+    SUFFIX_A="${VERSION_A[@]:1}"
+    VERSION_SUFFIX=$(join_by - ${SUFFIX_A})
+    dotnet pack -c Release NFlags/NFlags.csproj /p:Version=${TAG} --version-suffix ${VERSION_SUFFIX} -o $(pwd)
+fi
